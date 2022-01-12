c5951ecf263f1ad9f844a8844197033a7514bf9d
Lauri Koikkalainen <lauri.koikkalainen@ntnu.no>
2022-01-12 15:58:39 +0100

    did some stuff

---
+++ get_names.py
@@ -0,0 +1,15 @@
+import os
+from traitlets.config import get_config
+from nbgrader.apps import NbGraderAPI
+
+c = get_config()
+c.CourseDirectory.course_id = os.environ["RELEASE_NAME"]
+c.CourseDirectory.root = os.environ["HOME"] + "/courses/" + os.environ["RELEASE_NAME"]
+
+api = NbGraderAPI(config=c)
+
+assignments = []
+
+for i in api.get_assignments():
+    assignments.append(i["name"])
+    print(i["name"])
