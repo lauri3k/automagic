7e8181b802432ddac894bc161a8e5904a0d8bffb
Lauri Koikkalainen <lauri.koikkalainen@ntnu.no>
2023-08-18 09:11:38 +0200

    build

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker buildx build --platform linux/amd64,linux/arm64 -t quay.io/ntnu/autograde:v0.06 --push .
+docker buildx build --platform linux/amd64,linux/arm64 -t quay.io/ntnu/autograde:v0.07 --push .
